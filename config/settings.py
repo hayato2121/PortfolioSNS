c263521a6896f162bb337fd08569c6cbd7b62a28
hayato <nanalove.hayato1220@icloud.com>
2023-03-24 22:20:04 +0900

    デプロイ

--- config/settings.py
+++ config/settings.py
@@ -190,4 +190,8 @@ DATABASES = {
 }
 
 import django_heroku
+import dj_database_url
 django_heroku.settings(locals() )
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
