f47dd178c0eae0fab3315594b90b18c3d4824e49
hayato <nanalove.hayato1220@icloud.com>
2023-03-24 20:13:33 +0900

    デプロイ

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
     "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
