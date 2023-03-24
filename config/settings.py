f98ac7eb2399a39e9cb23eab0ea97acce80f4024
hayato <nanalove.hayato1220@icloud.com>
2023-03-24 21:05:36 +0900

    デプロイ

--- config/settings.py
+++ config/settings.py
@@ -103,8 +103,6 @@ dotenv_path=join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SUPERUSER_NAME = env("SUPERUSER_NAME")
@@ -148,6 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
