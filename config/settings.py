afcf8a216d6c9f09603e1eb63351210e6871dd66
hayato <nanalove.hayato1220@icloud.com>
2023-03-24 21:37:08 +0900

    デプロイ設定

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
 import os
 
 import environ
@@ -149,12 +150,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    (BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
+django_heroku.settings(locals(), staticfiles=False)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
