f6ae62891cfe3b2167bf1d330b15ea0a16707a43
hayato <nanalove.hayato1220@icloud.com>
2023-03-24 23:48:05 +0900

    デプロイ

--- config/settings.py
+++ config/settings.py
@@ -182,9 +182,9 @@ ACCOUNT_EMAIL_REQUIRED = True    #メールアドレスを必須項目に指定
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': dj_database_url.config()
 }
