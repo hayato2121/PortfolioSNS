944dc58e68ab4d2b3f9f1850239fa0c3c3da9c5d
hayato <nanalove.hayato1220@icloud.com>
2023-03-26 01:00:39 +0900

    デプロイ

--- config/settings.py
+++ config/settings.py
@@ -145,8 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/config/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 MEDIA_URL = '/media/'
