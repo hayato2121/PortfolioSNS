3ca1813d768b870e8a8ae6e100de1073d705a123
hayato <nanalove.hayato1220@icloud.com>
2023-03-26 15:44:25 +0900

    設定

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (
 
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR / "media")
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 
 # Default primary key field type
